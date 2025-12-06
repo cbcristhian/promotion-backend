8d3f22d9aac477a5da36c3519a455c2117fa84e1
cbcristhian <cristhian.castro@unosquare.com>
2025-12-05 21:22:43 -0400

    basic in-memory-data

---
+++ data/store/users.js
@@ -0,0 +1,12 @@
+//In-memory data
+const users = [{
+  id: "32c9a760-371c-4479-a828-6a8a76955089",
+  name: "Cris",
+  email: "cris@email.com",
+  role: "ADMIN",
+  password: "$2b$10$R4zP.AaNaedcongPhVsQ.OkUhmfjDYryoIubp7bRejFWfipJ2rtHi"
+}];
+
+module.exports = {
+  users,
+};
